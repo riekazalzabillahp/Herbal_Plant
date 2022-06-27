1bdc7b29c76f58a4c087a2a48b318481ad1638a8
Irfan Ripat <iripat@waresix.com>
2022-06-27 15:55:29 +0800

    save bitmap from preview so image ratio can be 1:1

--- app/src/main/java/com/rieka/herbaldetector/DetectorActivity.kt
+++ app/src/main/java/com/rieka/herbaldetector/DetectorActivity.kt
@@ -1,29 +1,24 @@
 package com.rieka.herbaldetector
 
 import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
-import androidx.camera.core.AspectRatio
-import androidx.camera.core.AspectRatio.RATIO_16_9
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
-import androidx.core.net.toUri
 import com.google.android.material.snackbar.Snackbar
 import com.google.common.util.concurrent.ListenableFuture
 import com.rieka.herbaldetector.databinding.ActivityDetectorBinding
-import java.io.ByteArrayOutputStream
 import java.io.File
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -69,11 +64,6 @@ class DetectorActivity : AppCompatActivity() {
                 animateFlash()
             }
         }
-
-//        binding.galleryBtn.setOnClickListener {
-//            val intent = Intent(this, GalleryActivity::class.java)
-//            startActivity(intent)
-//        }
     }
 
     private fun startCamera() {
@@ -94,29 +84,41 @@ class DetectorActivity : AppCompatActivity() {
         }, ContextCompat.getMainExecutor(this))
     }
 
+
+//    private fun takePhoto() {
+//        imageCapture?.let {
+//            val fileName = "JPEG_${System.currentTimeMillis()}.jpg"
+//            val file = File(externalMediaDirs[0], fileName)
+//            val outputFileOptions = ImageCapture.OutputFileOptions.Builder(file).build()
+//            it.takePicture(
+//                outputFileOptions,
+//                imgCaptureExecutor,
+//                object : ImageCapture.OnImageSavedCallback {
+//                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+//                        Log.i(TAG, "The image has been saved in ${file.toUri()}")
+//                    }
+//
+//                    override fun onError(exception: ImageCaptureException) {
+//                        Toast.makeText(
+//                            binding.root.context,
+//                            "Error taking photo",
+//                            Toast.LENGTH_LONG
+//                        ).show()
+//                        Log.d(TAG, "Error taking photo:$exception")
+//                    }
+//
+//                })
+//        }
+//    }
+
     private fun takePhoto() {
-        imageCapture?.let {
-            val fileName = "JPEG_${System.currentTimeMillis()}.jpg"
-            val file = File(externalMediaDirs[0], fileName)
-            val outputFileOptions = ImageCapture.OutputFileOptions.Builder(file).build()
-            it.takePicture(
-                outputFileOptions,
-                imgCaptureExecutor,
-                object : ImageCapture.OnImageSavedCallback {
-                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                        Log.i(TAG, "The image has been saved in ${file.toUri()}")
-                    }
+        binding.camera.bitmap?.let { File(externalMediaDirs[0], "bitmap_test.jpg").writeBitmap(it, Bitmap.CompressFormat.JPEG, 100) }
+    }
 
-                    override fun onError(exception: ImageCaptureException) {
-                        Toast.makeText(
-                            binding.root.context,
-                            "Error taking photo",
-                            Toast.LENGTH_LONG
-                        ).show()
-                        Log.d(TAG, "Error taking photo:$exception")
-                    }
-
-                })
+    private fun File.writeBitmap(bitmap: Bitmap, format: Bitmap.CompressFormat, quality: Int) {
+        outputStream().use { out ->
+            bitmap.compress(format, quality, out)
+            out.flush()
         }
     }
 
@@ -130,33 +132,7 @@ class DetectorActivity : AppCompatActivity() {
         }, 100)
     }
 
-    private fun cropImage(bitmap: Bitmap, frame: View, reference: View): ByteArray {
-        val heightOriginal = frame.height
-        val widthOriginal = frame.width
-        val heightFrame = reference.height
-        val widthFrame = reference.width
-        val leftFrame = reference.left
-        val topFrame = reference.top
-        val heightReal = bitmap.height
-        val widthReal = bitmap.width
-        val widthFinal = widthFrame * widthReal / widthOriginal
-        val heightFinal = heightFrame * heightReal / heightOriginal
-        val leftFinal = leftFrame * widthReal / widthOriginal
-        val topFinal = topFrame * heightReal / heightOriginal
-        val bitmapFinal = Bitmap.createBitmap(
-            bitmap,
-            leftFinal, topFinal, widthFinal, heightFinal
-        )
-        val stream = ByteArrayOutputStream()
-        bitmapFinal.compress(
-            Bitmap.CompressFormat.JPEG,
-            100,
-            stream
-        ) //100 is the best quality possibe
-        return stream.toByteArray()
-    }
-
     companion object {
-        val TAG = "MainActivity"
+        const val TAG = "MainActivity"
     }
 }
